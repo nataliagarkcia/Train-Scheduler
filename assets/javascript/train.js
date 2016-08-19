756fc8aac89131cd04e9956aa7f8355cf394a48b
nataliagarcia <nataliagarkcia@hotmail.com>
2016-08-18 22:40:00 -0400

    last changes javascript

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,36 +1,89 @@
+
+
+
+
 //determines that the train counter start with 0
 var trainCount = 0;
 
-$(document).on("click", function(){
+//global array that will keep all the inputs / objects
+var trainItem = [];
+
+
+//create a on click function that will run when click on search button
+$('#runSearch').on("click", function(){
 
     //grabs the users inputs in each of the followings and asigns variables to each one
-    var tName = $ ("#TrainName").val().trim();
+
+    var tName = $("#TrainName").val().trim();
     var dest = $ ("#destination").val().trim();
     var firstTrainT = $ ("#firstTrainTime").val().trim();
     var freq = $ ('#frequency').val().trim();
+	
 
- 
 
-    //console logs all the previous inputs    
-    console.log(tName);
-    console.log(dest);
-    console.log(firstTrainT);
-    console.log(freq);
-
-	 //store the values inside the local storage
-	 localStorage.setItem("name",  tName);
-	 localStorage.setItem("destination",  dest);
-	 localStorage.setItem("train time",  firstTrainT);
-	 localStorage.setItem("frequency", freq);
-
-	 //empty the values
-	  tName = $('#trainName').val("");
-	  dest = $('#place').val("");
-	  firstTrainT = $('#trainTime').val("");
-	  freq = $('#freq').val("");
-
-	 
-	return false;
-	});
 
 	
+    //im storing the data inside the local storage
+	localStorage.setItem("name",tName );
+	localStorage.setItem("destination",dest );
+	localStorage.setItem("frecuency",freq );
+	localStorage.setItem("firstTrain",firstTrainT );
+
+	//2var tr = $('<tr>')
+
+
+	
+
+
+	// First Time (pushed back 1 year to make sure it comes before current time)
+	//var firstTimeConverted = moment(train.firstTrainT,"hh:mm");
+	//console.log(firstTimeConverted);
+
+		// Current Time
+	//var currentTime = moment();
+	//console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+	//difference time
+	//var diffTime = moment().diff(moment(train.firstTrainT), "minutes");
+
+	// Time apart (remainder)
+	//var tRemainder = diffTime % (train.freq) ;
+	//console.log(tRemainder)
+
+	//var MinutesTillTrain = (train.freq) - tRemainder;
+	//console.log("MINUTES TILL TRAIN: " + MinutesTillTrain);
+
+	// Next Train
+	//var nextTrain = moment().add(MinutesTillTrain, "minutes")
+	//console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm")) 
+
+
+
+     //store the values inside the local storage
+    
+
+    retrieve();
+
+     //empty the values
+    tName = $('#TrainName').val("");
+    dest = $('#destination').val("");
+    firstTrainT = $('#firstTrainTime').val("");
+    freq = $('#frequency').val("");
+    
+    trainCount ++;
+
+     
+    return false;
+    });
+
+
+//retrieves the values from the local storage
+function retrieve () {
+ 
+
+    $('#display').append('<tr>')
+    for (var i = 0; i < localStorage.length; i++) { 
+        console.log("HERE")
+    $('#display').append( '<td>' + localStorage.getItem(localStorage.key(i)) + '</td>');
+    }
+    $('#display').append('</tr>')
+}
